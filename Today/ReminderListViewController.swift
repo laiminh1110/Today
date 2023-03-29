cfc711c82e525a31d16c7a215792bc4d88223235
minhl <minhl@scb.com.vn>
2023-03-29 17:30:32 +0700

    Configure the data source

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -9,26 +9,44 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
     
+    //Add a type alias for the diffable data source.
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    var dataSource:DataSource! // The object you use to manage data and provide cells for a collection view.
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        self.configClsViewLayout()
-    }
+         super.viewDidLoad()
+
+         let listLayout = listLayout()
+         collectionView.collectionViewLayout = listLayout
+
+        //Create a new cell registration.
+         let cellRegistration = UICollectionView.CellRegistration {
+             (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+             let reminder = Reminder.sampleData[indexPath.item]
+             var contentConfiguration = cell.defaultContentConfiguration()
+             contentConfiguration.text = reminder.title
+             cell.contentConfiguration = contentConfiguration
+         }
+        
+        
+        // connect the diffable data source to the collection view
+        dataSource = DataSource(collectionView: collectionView) {
+                  (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+                  return collectionView.dequeueConfiguredReusableCell(
+                      using: cellRegistration, for: indexPath, item: itemIdentifier)
+              }
+     }
     
-    
-    // collectionView Layout
-    private func configClsViewLayout(){
-        let listLayout = listLayout()
-        collectionView.collectionViewLayout = listLayout
-    }
-    
-    // Configure the collection as a list
-    private func listLayout() -> UICollectionViewCompositionalLayout{
-        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
-        listConfiguration.showsSeparators = true
-        listConfiguration.backgroundColor = .clear
-        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
-    }
-    
-    
+
+    //Configure the collection as a list
+     private func listLayout() -> UICollectionViewCompositionalLayout {
+         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+         listConfiguration.showsSeparators = true
+         listConfiguration.backgroundColor = .clear
+         return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+     }
 }
 
+
+
+//
