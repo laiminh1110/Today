1605d8f622a76822c1ffa3bff3e5c82231c59a1f
minhl <minhl@scb.com.vn>
2023-03-30 15:43:51 +0700

    Creating a list view

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -6,47 +6,71 @@
 //https://developer.apple.com/tutorials/app-dev-training/creating-a-list-view
 
 import UIKit
-
+/**
+    - Create a list view in UICollection view
+            + Define item identifier type (reminder model)
+            + Define section indentifier type
+            + Configure the collection view with List layout
+            + Define how data in shown using cell registration
+            + Define the collection view 's data source
+            + create and apply a snapshot to the data source
+ */
 class ReminderListViewController: UICollectionViewController {
     
     //Add a type alias for the diffable data source.
     typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+
     var dataSource:DataSource! // The object you use to manage data and provide cells for a collection view.
     
     override func viewDidLoad() {
-         super.viewDidLoad()
-
-         let listLayout = listLayout()
-         collectionView.collectionViewLayout = listLayout
-
-        //Create a new cell registration.
-         let cellRegistration = UICollectionView.CellRegistration {
-             (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
-             let reminder = Reminder.sampleData[indexPath.item]
-             var contentConfiguration = cell.defaultContentConfiguration()
-             contentConfiguration.text = reminder.title
-             cell.contentConfiguration = contentConfiguration
-         }
+        super.viewDidLoad()
         
+        //Assign the list layout to the collection view layout.
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
         
+        //// Create cell registration that defines how data should be shown in a cell
+        let cellRegistration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            
+            // Define how data should be shown using content configuration
+            var contentConfiguration = cell.defaultContentConfiguration()
+                contentConfiguration.text = reminder.title
+            
+            // Assign content configuration to cell
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+
         // connect the diffable data source to the collection view
         dataSource = DataSource(collectionView: collectionView) {
-                  (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
-                  return collectionView.dequeueConfiguredReusableCell(
-                      using: cellRegistration, for: indexPath, item: itemIdentifier)
-              }
-     }
-    
-
-    //Configure the collection as a list
-     private func listLayout() -> UICollectionViewCompositionalLayout {
-         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
-         listConfiguration.showsSeparators = true
-         listConfiguration.backgroundColor = .clear
-         return UICollectionViewCompositionalLayout.list(using: listConfiguration)
-     }
-}
-
-
-
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+        
+        
+        var snapShot = Snapshot()
+            snapShot.appendSections([0])
+            snapShot.appendItems(Reminder.sampleData.map{$0.title} )
+        
+//        var reminderTitles = [String]()
+//        for remider in Reminder.sampleData{
+//            reminderTitles.append(remider.title)
+//        }
 //
+//        snapShot.appendItems(reminderTitles)
+        
+        dataSource.apply(snapShot)
+    }
+    
+    // Created List Layout
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = true
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
