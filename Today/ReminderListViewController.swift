62cd15bba60800e36a74f546c1150c0bfaaa6a4e
minhl <minhl@scb.com.vn>
2023-03-31 10:17:02 +0700

    add comment in remider list

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -7,20 +7,20 @@
 
 import UIKit
 /**
-    - Create a list view in UICollection view
-            + Define item identifier type (reminder model)
-            + Define section indentifier type
-            + Configure the collection view with List layout
-            + Define how data in shown using cell registration
-            + Define the collection view 's data source
-            + create and apply a snapshot to the data source
+ - Create a list view in UICollection view
+ + Define item identifier type (reminder model)
+ + Define section indentifier type
+ + Configure the collection view with List layout
+ + Define how data in shown using cell registration
+ + Define the collection view 's data source
+ + create and apply a snapshot to the data source
  */
 class ReminderListViewController: UICollectionViewController {
     
     //Add a type alias for the diffable data source.
     typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
-
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String> //A representation of the state of the data in a view at a specific point in time.
+    
     var dataSource:DataSource! // The object you use to manage data and provide cells for a collection view.
     
     override func viewDidLoad() {
@@ -37,32 +37,29 @@ class ReminderListViewController: UICollectionViewController {
             
             // Define how data should be shown using content configuration
             var contentConfiguration = cell.defaultContentConfiguration()
-                contentConfiguration.text = reminder.title
+            contentConfiguration.text = reminder.title
+            contentConfiguration.secondaryText = reminder.title
             
             // Assign content configuration to cell
             cell.contentConfiguration = contentConfiguration
         }
         
-
+        
         // connect the diffable data source to the collection view
         dataSource = DataSource(collectionView: collectionView) {
             (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
-            return collectionView.dequeueConfiguredReusableCell(
-                using: cellRegistration, for: indexPath, item: itemIdentifier)
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+            // =>>>>>>>> Configure and return cell.
         }
         
+
         
+        //// Create a snapshot.
         var snapShot = Snapshot()
-            snapShot.appendSections([0])
-            snapShot.appendItems(Reminder.sampleData.map{$0.title} )
-        
-//        var reminderTitles = [String]()
-//        for remider in Reminder.sampleData{
-//            reminderTitles.append(remider.title)
-//        }
-//
-//        snapShot.appendItems(reminderTitles)
-        
+        // // Populate the snapshot.
+        snapShot.appendSections([0])
+        snapShot.appendItems(Reminder.sampleData.map{$0.title} )
+        // Apply the snapshot.
         dataSource.apply(snapShot)
     }
     
